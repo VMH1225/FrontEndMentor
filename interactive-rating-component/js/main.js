c6b71ed0e60ef0e9fd820d5f4edce642cba066f2
VMH1225 <huertavictor1225@gmail.com>
2022-11-11 07:59:51 -0800

    added initial js for thank you state

--- interactive-rating-component/js/main.js
+++ interactive-rating-component/js/main.js
@@ -4,4 +4,7 @@ document.querySelector("#submit").addEventListener('click', disappear)
 
 function disappear(){
     document.querySelector('main').style.display = 'none'
+    document.querySelector('.thank-you').style.display = 'flex'
+    document.querySelector('#selection').innerText = 'You selected 4 out of 5'
+
 }

13e83356496aad6e5031e523151887f186a08dbc
VMH1225 <huertavictor1225@gmail.com>
2022-11-12 11:53:19 -0800

    inital js added event listeners to buttons

--- interactive-rating-component/js/main.js
+++ interactive-rating-component/js/main.js
@@ -1,10 +1,16 @@
 
-
+let choice = 0
 document.querySelector("#submit").addEventListener('click', disappear)
+document.querySelector(".num").addEventListener('click', setValue)
 
 function disappear(){
     document.querySelector('main').style.display = 'none'
     document.querySelector('.thank-you').style.display = 'flex'
-    document.querySelector('#selection').innerText = 'You selected 4 out of 5'
-
+    document.querySelector('#selection').innerText = `You selected ${choice} out of 5`
 }
+
+
+function setValue(){
+    choice = document.querySelector(".num").innerText
+}
+

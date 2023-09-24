d37e98149fedec4e98e7e05e2a09a486d0c4fce6
VMH1225 <huertavictor1225@gmail.com>
2023-09-24 10:47:19 -0700

    on click of share button show socials section

--- article-preview-component-master/js/main.js
+++ article-preview-component-master/js/main.js
@@ -1,1 +1,7 @@
 
+document.querySelector('.share').addEventListener('click', displaySocials)
+
+function displaySocials(){
+    total = 0
+    document.querySelector('.socials').style.display = 'flex'
+}

21fbd00a6f3185277742309170ab047ddced7ebd
VMH1225 <huertavictor1225@gmail.com>
2023-09-30 17:00:29 -0700

    change path of background images in js file

--- article-preview-component-master/js/main.js
+++ article-preview-component-master/js/main.js
@@ -5,17 +5,17 @@ var num = 2
 function displaySocials(){
     if(num % 2 == 0){
     document.querySelector('.socials').style.display = 'flex'
-    document.querySelector('.share').style.backgroundImage = 'url("../images/icon-shareActive.svg")'
+    document.querySelector('.share').style.backgroundImage = 'url("images/icon-shareActive.svg")'
     document.querySelector('.share').style.backgroundColor = 'hsl(214, 17%, 51%)'
-    document.querySelector('#mobileShare').style.backgroundImage = 'url("../images/icon-shareActive.svg")'
+    document.querySelector('#mobileShare').style.backgroundImage = 'url("images/icon-shareActive.svg")'
     document.querySelector('#mobileShare').style.backgroundColor = 'hsl(214, 17%, 51%)'
     num++
     }
     else{
         document.querySelector('.socials').style.display = 'none'
-        document.querySelector('.share').style.backgroundImage = 'url("../images/icon-share.svg")'
+        document.querySelector('.share').style.backgroundImage = 'url("images/icon-share.svg")'
         document.querySelector('.share').style.backgroundColor = 'hsl(210, 46%, 95%)'
-        document.querySelector('#mobileShare').style.backgroundImage = 'url("../images/icon-share.svg")'
+        document.querySelector('#mobileShare').style.backgroundImage = 'url("images/icon-share.svg")'
         document.querySelector('#mobileShare').style.backgroundColor = 'hsl(210, 46%, 95%)'
         num++
     }
